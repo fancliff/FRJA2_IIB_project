77953798bc0a86b05c1d68ea46680aa50237d50c
Freddie Ancliff <frja2@cam.ac.uk>
2024-10-28 17:40:51 +0000

    missing latest data generation function fromt test.py

--- simple_CNN_magnitude.py
+++ simple_CNN_magnitude.py
@@ -6,62 +6,68 @@ from torch.utils.data import DataLoader, Dataset
 import numpy as np
 import scipy
 from numba import jit, types
+import matplotlib.pyplot as plt
 
 @jit(nopython=True)
-class simple_dataset(Dataset):
-    def __init__(self, num_samples=100, signal_length=400):
-        self.num_samples = num_samples
-        self.signal_length = signal_length
-        self.data = []
-        self.labels = []
+def generate_data(num_samples=1000, signal_length=400):
+    
+    data = np.empty(num_samples,dtype=np.float64)
+    labels = np.empty(num_samples)
+    
+    frequencies = np.linspace(0,1,signal_length)
+    for i in range(num_samples):
+        alphas = []
+        zetas = []
+        omegas = []
+        H_v = np.zeros(signal_length,dtype=types.complex128)
+        label = np.zeros(signal_length)
         
-        frequencies = np.linspace(0,1,signal_length)
-        for _ in range(num_samples):
-            alphas = []
-            zetas = []
-            omegas = []
-            H_v = np.zeros(signal_length,dtype=types.complex128)
-            label = np.zeros(signal_length)
-            
-            num_modes = np.random.randint(1,5)
-            #when noise is added change num_modes to include 0
+        num_modes = np.random.randint(1,5)
+        #when noise is added change num_modes to include 0
+        for n in range(num_modes):
+            # to improve model add a random sign to alpha_j 
+            alphas.append(np.random.uniform(1,2))
+            zetas.append(np.exp(np.random.uniform(np.log(0.01), np.log(0.1))))
+            #no modes at very edge of frequency range
+            omegas.append(np.random.uniform(0.001,0.999))
+        
+        for i, w in enumerate(frequencies):
+            H_f = 0.0j
             for n in range(num_modes):
                 # to improve model add a random sign to alpha_j 
-                alphas.append(np.random.uniform(1,2))
-                zetas.append(np.exp(np.random.uniform(np.log(0.01), np.log(0.1))))
-                #no modes at very edge of frequency range
-                omegas.append(np.random.uniform(0.001,0.999))
+                alpha_n = alphas[n]
+                zeta_n = zetas[n]
+                omega_n = omegas[n]
                 
+                denominator = omega_n**2 - w**2 + 2j * zeta_n * w
+                numerator = 1j*w*alpha_n
+                H_f += numerator/denominator
+                
+                #set label bandwidth to 3db or arbitrary value
+                #bandwidth = 0.02 #(8 frequency points wide if 400 points)
+                bandwidth = 2*omega_n*zeta_n
+                label[(frequencies >= omega_n - bandwidth) & (frequencies <= omega_n + bandwidth)] = 1
+                
+            H_v[i] = H_f
             
-            for i, w in enumerate(frequencies):
-                H_f = 0.0j
-                for n in range(num_modes):
-                    # to improve model add a random sign to alpha_j 
-                    alpha_n = alphas[n]
-                    zeta_n = zetas[n]
-                    omega_n = omegas[n]
-                    
-                    denominator = omega_n**2 - w**2 + 2j * zeta_n * w
-                    numerator = 1j*w*alpha_n
-                    H_f += numerator/denominator
-                    
-                    #set label bandwidth to 3db or arbitrary value
-                    #bandwidth = 0.02 #(8 frequency points wide if 400 points)
-                    bandwidth = 2*omega_n*zeta_n
-                    label[(frequencies >= omega_n - bandwidth) & (frequencies <= omega_n + bandwidth)] = 1
-                    
-                H_v[i] = H_f
-                
-            self.data.append(np.abs(H_v)) #use signal magnitude for now
-            self.labels.append(label)
+        data[i] = np.abs(H_v) #use signal magnitude for now
+        labels[i] = label
+    return data, labels
+
+class simple_dataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
     
     def __len__(self):
-        return self.num_samples
+        return len(self.labels)
     
-    def __getitem__(self, index):
-        return self.data[index], self.labels[index]
-    
-class PeakMagCNN(nn.module):
+    def __getitem__(self, idx):
+        signal = torch.tensor(self.data[idx], dtype=torch.float32)
+        label = torch.tensor(self.labels[idx], dtype=torch.int)
+        return signal, label
+
+class PeakMagCNN(nn.Module):
     def __init__(self):
         super(PeakMagCNN, self).__init__()
         self.conv1 = nn.Conv1d(1, 16, kernel_size=5, padding=2)
@@ -100,8 +106,21 @@ class PeakMagCNN(nn.module):
         for s in size:
             num_features *= s
         return num_features
-    
-dataset = simple_dataset(num_samples=1000, signal_length=400)
+
+# Generate vibration data
+data, labels = generate_data(num_samples=1000, signal_length=400)
+
+# Visualize a sample signal with labels
+plt.figure(figsize=(12, 6))
+plt.plot(data[0], label='Signal')
+plt.plot(labels[0] * 5, label='Labels (scaled)', linestyle='--')  # Scale labels for visibility
+plt.title('Sample Vibration Signal with Labels')
+plt.xlabel('Normalised Frequency')
+plt.ylabel('Amplitude / Label')
+plt.legend()
+plt.show()
+
+dataset = simple_dataset(data, labels)
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 model = PeakMagCNN()
