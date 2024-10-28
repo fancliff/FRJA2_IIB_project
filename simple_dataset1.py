75a2f1c0291cd85b356289bd9889884a988cd69c
Freddie Ancliff <frja2@cam.ac.uk>
2024-10-28 13:20:34 +0000

    added labels from test.py into CNN file

--- simple_dataset1.py
+++ simple_dataset1.py
@@ -12,14 +12,42 @@ class simple_dataset(Dataset):
         self.data = []
         self.labels = []
         
+        frequencies = np.linspace(0,1,signal_length)
         for _ in range(num_samples):
-            num_modes = np.random.randint(0,5)
+            alphas = []
+            zetas = []
+            omegas = []
             H_v = np.zeros(signal_length,dtype=np.complex128)
-            for _ in range(num_modes):
-                # to improve model add a random sign to alpha_j 
-                alpha_j = np.random.uniform(1,2)
-                zeta_j = scipy.stats.loguniform(0.01,0.2)
-                omega_j = np.random.uniform(0,1)
-                for w in range(1/signal_length):
-                    print(w)
+            label = np.zeros(signal_length,dtype=int)
             
+            num_modes = np.random.randint(1,5)
+            #when noise is added change num_modes to include 0
+            for n in range(num_modes):
+                # to improve model add a random sign to alpha_j 
+                alphas.append(np.random.uniform(1,2))
+                zetas.append(scipy.stats.loguniform.rvs(0.01,0.2))
+                #no modes at very edge of frequency range
+                omegas.append(np.random.uniform(0.001,0.999))
+                
+            
+            for i, w in enumerate(frequencies):
+                H_f = 0.0j
+                for n in range(num_modes):
+                    # to improve model add a random sign to alpha_j 
+                    alpha_n = alphas[n]
+                    zeta_n = zetas[n]
+                    omega_n = omegas[n]
+                    
+                    denominator = omega_n**2 - w**2 + 2j * zeta_n * w
+                    numerator = 1j*w*alpha_n
+                    H_f += numerator/denominator
+                    
+                    #set label bandwidth to 3db or arbitrary value
+                    #bandwidth = 0.02 #(8 frequency points wide if 400 points)
+                    bandwidth = 2*omega_n*zeta_n
+                    label[(frequencies >= omega_n - bandwidth) & (frequencies <= omega_n + bandwidth)] = 1
+                    
+                H_v[i] = H_f
+                
+            self.data.append(np.abs(H_v)) #use signal magnitude for now
+            self.labels.append(label)
