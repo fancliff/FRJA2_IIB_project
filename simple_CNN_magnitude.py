2b5019e1ca0f438d72a096556083c52f1c76544e
Freddie Ancliff <frja2@cam.ac.uk>
2024-10-28 18:14:44 +0000

    added latest signal gen function from signal_gen.py  into CNN file

--- simple_CNN_magnitude.py
+++ simple_CNN_magnitude.py
@@ -11,47 +11,48 @@ import matplotlib.pyplot as plt
 @jit(nopython=True)
 def generate_data(num_samples=1000, signal_length=400):
     
-    data = np.empty(num_samples,dtype=np.float64)
-    labels = np.empty(num_samples)
-    
+    data = np.empty((num_samples,signal_length),dtype=np.float64)
+    labels = np.empty((num_samples,signal_length),dtype=np.int32)
+
     frequencies = np.linspace(0,1,signal_length)
     for i in range(num_samples):
-        alphas = []
-        zetas = []
-        omegas = []
-        H_v = np.zeros(signal_length,dtype=types.complex128)
-        label = np.zeros(signal_length)
         
-        num_modes = np.random.randint(1,5)
+        H_v = np.zeros(signal_length, dtype=np.complex128)
+        label = np.zeros(signal_length, dtype=np.int32)
+        
+        #max 5 modes
+        num_modes = np.random.randint(1,6)
         #when noise is added change num_modes to include 0
+        # to improve model add a random sign to alpha_j 
+        alphas = np.random.uniform(1, 2, size=5)
+        zetas = np.exp(np.random.uniform(np.log(0.01), np.log(0.1), size=5))
+        #no modes at very edge of frequency range
+        omegas = np.random.uniform(0.001, 0.999, size=5)
+        
         for n in range(num_modes):
             # to improve model add a random sign to alpha_j 
-            alphas.append(np.random.uniform(1,2))
-            zetas.append(np.exp(np.random.uniform(np.log(0.01), np.log(0.1))))
-            #no modes at very edge of frequency range
-            omegas.append(np.random.uniform(0.001,0.999))
-        
-        for i, w in enumerate(frequencies):
-            H_f = 0.0j
-            for n in range(num_modes):
-                # to improve model add a random sign to alpha_j 
-                alpha_n = alphas[n]
-                zeta_n = zetas[n]
-                omega_n = omegas[n]
+            alpha_n = alphas[n]
+            zeta_n = zetas[n]
+            omega_n = omegas[n]
+            
+            for j, w in enumerate(frequencies):
+                H_f = 0.0j
                 
                 denominator = omega_n**2 - w**2 + 2j * zeta_n * w
                 numerator = 1j*w*alpha_n
+                
                 H_f += numerator/denominator
                 
-                #set label bandwidth to 3db or arbitrary value
-                #bandwidth = 0.02 #(8 frequency points wide if 400 points)
-                bandwidth = 2*omega_n*zeta_n
-                label[(frequencies >= omega_n - bandwidth) & (frequencies <= omega_n + bandwidth)] = 1
-                
-            H_v[i] = H_f
+                H_v[j] += H_f
             
-        data[i] = np.abs(H_v) #use signal magnitude for now
-        labels[i] = label
+            #set label bandwidth to 3db or arbitrary value
+            #bandwidth = 0.02 #(8 frequency points wide if 400 points)
+            bandwidth = 2*omega_n*zeta_n
+            label[(frequencies >= omega_n - bandwidth) & (frequencies <= omega_n + bandwidth)] = 1
+        
+        data[i, :] = np.abs(H_v) #use signal magnitude for now
+        labels[i, :] = label
+        
     return data, labels
 
 class simple_dataset(Dataset):
