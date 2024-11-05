b6d058fc84f9e4cab80a9da848b2343376c11944
Freddie Ancliff <frja2@cam.ac.uk>
2024-11-05 10:59:17 +0000

    testing noisy signals

--- run_area.py
+++ run_area.py
@@ -8,30 +8,33 @@ import scipy
 import matplotlib.pyplot as plt
 from numba import jit
 
-from generators import mag_1D_no_noise as generate_data
+from generators import mag_1D_noise as generate_data
 from models import simple_dataset, PeakMag1, PeakMag2
-from routines import train_model_binary, plot_losses, compare_models, load_model
+from routines import train_model_binary, compare_models, load_model, plot_loss_history, plot_precision_history, plot_recall_history, plot_samples
 
-data, labels = generate_data(num_samples=10000, signal_length=1024)
+data, labels = generate_data(num_samples=4000, signal_length=1024)
 train_dataset = simple_dataset(data, labels)
 train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 
-data, labels = generate_data(num_samples=1000, signal_length=1024)
+data, labels = generate_data(num_samples=400, signal_length=1024)
 val_dataset = simple_dataset(data, labels)
 val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=True)
+
+plot_samples(train_dataloader, 10)
 
 model1 = PeakMag1()
 model2 = PeakMag2()
 
 plot_during = False
-plot_after = True
+
+'''
 
 result_dict1 = train_model_binary(
                 model1, 
                 train_dataloader, 
                 val_dataloader, 
                 save_name='PeakMag1_2', #None if no save required
-                num_epochs = 20, 
+                num_epochs = 6, 
                 acceptance=0.5, 
                 plotting=plot_during
                 )
@@ -41,13 +44,14 @@ result_dict2 = train_model_binary(
                 train_dataloader, 
                 val_dataloader, 
                 save_name='PeakMag2_1', #None if no save required
-                num_epochs = 10, 
+                num_epochs = 6, 
                 acceptance=0.5, 
                 plotting=plot_during
                 )
 
-if plot_after:
-    plot_losses([result_dict1,result_dict2],log_scale=True)
+plot_loss_history([result_dict1,result_dict2],log_scale=True)
+plot_precision_history([result_dict1,result_dict2],log_scale=True)
+plot_recall_history([result_dict1,result_dict2],log_scale=True)
 
 #load models from save files or train above
 #model1 = load_model('PeakMag1_1')
@@ -55,3 +59,5 @@ if plot_after:
 
 criterion=nn.BCEWithLogitsLoss()
 compare_models(model1, model2, val_dataloader, criterion, acceptance1=0.5, acceptance2=0.5)
+
+'''
