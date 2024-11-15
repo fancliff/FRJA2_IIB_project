fc8e50d4d65bf09f8795201af992da8836939f4d
Freddie Ancliff <frja2@cam.ac.uk>
2024-11-15 15:44:00 +0000

    reorder save and overwrite statements so that print statement in save doesn't just print the new best twice

--- models.py
+++ models.py
@@ -65,8 +65,8 @@ class EarlyStopping:
             self.best_loss = val_loss
             self.save_checkpoint(model, val_loss)
         elif val_loss < self.best_loss + self.delta:
-            self.best_loss = val_loss
             self.save_checkpoint(model, val_loss)
+            self.best_loss = val_loss
             self.counter = 0
         else:
             self.counter += 1
