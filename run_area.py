3a758a0e40ba3075a01e621a39a4c24337e55cfe
Freddie Ancliff <frja2@cam.ac.uk>
2024-11-20 10:53:25 +0000

    testing of added convolutional layer ~ 3x more parameters

--- run_area.py
+++ run_area.py
@@ -17,7 +17,7 @@ import time
 
 #                      mag, real, imag, phase, log_mag
 outputs1 = np.array([False, True, True, False, False])
-outputs2 = np.array([False, True, True, True, False])
+outputs2 = np.array([False, True, True, False, False])
 
 data, labels = n_channels_gen(num_samples=4000, signal_length=1024, sigma_max=0.1, min_max=True, enabled_outputs=outputs1)
 train_dataset_1 = md.n_channel_dataset(data, labels)
@@ -27,7 +27,7 @@ data, labels = n_channels_gen(num_samples=400, signal_length=1024, sigma_max=0.1
 val_dataset_1 = md.n_channel_dataset(data, labels)
 val_dataloader_1 = DataLoader(val_dataset_1, batch_size=32, shuffle=True)
 
-'''
+
 
 data, labels = n_channels_gen(num_samples=4000, signal_length=1024, sigma_max=0.1, min_max=True, enabled_outputs=outputs2)
 train_dataset_2 = md.n_channel_dataset(data, labels)
@@ -37,7 +37,7 @@ data, labels = n_channels_gen(num_samples=400, signal_length=1024, sigma_max=0.1
 val_dataset_2 = md.n_channel_dataset(data, labels)
 val_dataloader_2 = DataLoader(val_dataset_2, batch_size=32, shuffle=True)
 
-'''
+
 
 #rt.plot_samples(train_dataloader, 20)
 #rt.plot_samples(val_dataloader, 20)
@@ -47,10 +47,9 @@ val_dataloader_2 = DataLoader(val_dataset_2, batch_size=32, shuffle=True)
 
 model1 = md.PeakMag6(data_channels=np.sum(outputs1))
 print(f'Model 1 trainable parameters: {rt.count_parameters(model1)}')
-print(rt.count_parameters(model1))
 
-#model2 = md.PeakMag5(data_channels=np.sum(outputs2))
-#print(f'Model 2 trainable parameters: {rt.count_parameters(model2)}')
+model2 = md.PeakMag8(data_channels=np.sum(outputs2))
+print(f'Model 2 trainable parameters: {rt.count_parameters(model2)}')
 
 results = []
 
@@ -62,7 +61,7 @@ result_dict1,_ = rt.train_model_binary(
                 model1, 
                 train_dataloader_1, 
                 val_dataloader_1, 
-                save_name='PeakMag6_2', #None if no save required
+                save_name=None, #None if no save required
                 num_epochs = 100, 
                 acceptance=0.5, 
                 plotting=plot_during,
@@ -72,15 +71,15 @@ results.append(result_dict1)
 end1 = time.time()
 
 
-'''
+
 
 start2 = time.time()
 result_dict2,_ = rt.train_model_binary(
                 model2, 
                 train_dataloader_2, 
                 val_dataloader_2, 
-                save_name='PeakMag5_real_imag_phase', #None if no save required
-                num_epochs = 20, 
+                save_name='PeakMag8_1', #None if no save required
+                num_epochs = 100, 
                 acceptance=0.5, 
                 plotting=plot_during,
                 patience = 4,
@@ -88,21 +87,21 @@ result_dict2,_ = rt.train_model_binary(
 results.append(result_dict2)
 end2 = time.time()
 
-'''
+
 
 rt.plot_loss_history(results,log_scale=True)
 rt.plot_precision_history(results,log_scale=True)
 rt.plot_recall_history(results,log_scale=True)
 
 print('Time taken for model 1 training: ', end1-start1)
-#print('Time taken for model 2 training: ', end2-start2)
+print('Time taken for model 2 training: ', end2-start2)
 
 #load models from save files or train above
 #model1 = rt.load_model('PeakMag1_1')
 #model2 = rt.load_model('PeakMag5_real_imag')
 
-#criterion=nn.BCELoss()
-#rt.compare_models(model1, model2, val_dataloader_1, criterion, acceptance1=0.5, acceptance2=0.5)
+criterion=nn.BCELoss()
+rt.compare_models(model1, model2, val_dataloader_1, criterion, acceptance1=0.5, acceptance2=0.5)
 
 
 
