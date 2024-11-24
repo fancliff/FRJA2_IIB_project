5f8287c1d7702286dfd0b0bf2426fa695bafdc16
Freddie Ancliff <frja2@cam.ac.uk>
2024-11-24 17:14:45 +0000

    testing magnitude only against real and imaginary parts

--- run_area.py
+++ run_area.py
@@ -16,10 +16,10 @@ import time
 
 
 #                      mag, real, imag, phase, log_mag
-outputs1 = np.array([False, True, True, False, False])
+outputs1 = np.array([True, False, False, False, False])
 outputs2 = np.array([False, True, True, False, False])
 
-data, labels = n_channels_gen(num_samples=4000, signal_length=1024, sigma_min=0.01, sigma_max=0.1, min_max=True, enabled_outputs=outputs1)
+data, labels = n_channels_gen(num_samples=40000, signal_length=1024, sigma_min=0.01, sigma_max=0.1, min_max=True, enabled_outputs=outputs1)
 train_dataset_1 = md.n_channel_dataset(data, labels)
 train_dataloader_1 = DataLoader(train_dataset_1, batch_size=32, shuffle=True)
 
@@ -29,7 +29,7 @@ val_dataloader_1 = DataLoader(val_dataset_1, batch_size=32, shuffle=True)
 
 
 
-data, labels = n_channels_gen(num_samples=4000, signal_length=1024, sigma_min=0.01, sigma_max=0.1, min_max=True, enabled_outputs=outputs2)
+data, labels = n_channels_gen(num_samples=40000, signal_length=1024, sigma_min=0.01, sigma_max=0.1, min_max=True, enabled_outputs=outputs2)
 train_dataset_2 = md.n_channel_dataset(data, labels)
 train_dataloader_2 = DataLoader(train_dataset_2, batch_size=32, shuffle=True)
 
@@ -52,14 +52,14 @@ print(f'Model 2 trainable parameters: {rt.count_parameters(model2)}')
 results = []
 
 plot_during = False
-'''
+
 
 start1 = time.time()
 result_dict1,_ = rt.train_model_binary(
                 model1, 
                 train_dataloader_1, 
                 val_dataloader_1, 
-                save_name=None, #None if no save required
+                save_name='PeakMag5_40000_mag_only', #None if no save required
                 num_epochs = 100, 
                 acceptance=0.5, 
                 plotting=plot_during,
@@ -69,7 +69,7 @@ results.append(result_dict1)
 end1 = time.time()
 
 
-
+'''
 
 start2 = time.time()
 result_dict2,_ = rt.train_model_binary(
@@ -85,21 +85,21 @@ result_dict2,_ = rt.train_model_binary(
 results.append(result_dict2)
 end2 = time.time()
 
-
+'''
 
 rt.plot_loss_history(results, log_scale=True, show=False)
 rt.plot_precision_history(results, log_scale=False, show=False)
 rt.plot_recall_history(results, log_scale=False, show=False)
 
 print('Time taken for model 1 training: ', end1-start1)
-print('Time taken for model 2 training: ', end2-start2)
+#print('Time taken for model 2 training: ', end2-start2)
 
-'''
+
 
 
 #load models from save files or train above
-model1 = rt.load_model('PeakMag5_40000')
-model2 = rt.load_model('PeakMag5_4000')
+model1 = rt.load_model('PeakMag5_40000_mag_only')
+model2 = rt.load_model('PeakMag5_40000_real_imag')
 
 #rt.visualise_activations(model1, val_dataloader_1, 3)
 #rt.visualise_activations_with_signal(model1, val_dataloader_1, 3)
@@ -110,7 +110,7 @@ rt.compare_models(
     model1, 
     model2,
     val_dataloader_1,
-    val_dataloader_1,
+    val_dataloader_2,
     criterion,
     acceptance1=0.5,
     acceptance2=0.5,
